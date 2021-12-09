fc549e5162e6d677be4f7ccf23d45bc59388fd7d
Michael Palmer <mpalmer@singular.co.za>
2021-12-09 14:42:36 +0200

    Add example ServiceClient

---
+++ BlazorServerServiceClientExample/BlazorServerServiceClientExample/Contexts/ServiceClientContext.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorServerServiceClientExample.Contexts
+{
+  using Microsoft.PowerPlatform.Dataverse.Client;
+
+  public class ServiceClientContext
+  {
+    public ServiceClientContext()
+    {
+      string dataverseConnectionString = "<connection string here>";
+      this.ServiceClient = new ServiceClient(dataverseConnectionString);
+      if (!this.ServiceClient.IsReady)
+      {
+        throw this.ServiceClient.LastException;
+      }
+    }
+
+    private ServiceClient ServiceClient { get;set; }
+}
+}
--- BlazorServerServiceClientExample/BlazorServerServiceClientExample/Program.cs
+++ BlazorServerServiceClientExample/BlazorServerServiceClientExample/Program.cs
@@ -1,3 +1,4 @@
+using BlazorServerServiceClientExample.Contexts;
 using BlazorServerServiceClientExample.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -8,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddScoped<ServiceClientContext>();
 
 var app = builder.Build();
 
